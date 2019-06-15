bf0d095ce78a9b0ca13349ff479bb07822bab507
Eric Dumazet <edumazet@google.com>
2019-06-15 13:19:55 -0700

    tcp: fix compile error if !CONFIG_SYSCTL
    tcp_tx_skb_cache_key and tcp_rx_skb_cache_key must be available
    even if CONFIG_SYSCTL is not set.
    
    Fixes: 0b7d7f6b2208 ("tcp: add tcp_tx_skb_cache sysctl")
    Fixes: ede61ca474a0 ("tcp: add tcp_rx_skb_cache sysctl")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -57,11 +57,6 @@ static int tcp_delack_seg_max = 60;
 static int tcp_use_userconfig_min;
 static int tcp_use_userconfig_max = 1;
 
-DEFINE_STATIC_KEY_FALSE(tcp_rx_skb_cache_key);
-EXPORT_SYMBOL(tcp_rx_skb_cache_key);
-
-DEFINE_STATIC_KEY_FALSE(tcp_tx_skb_cache_key);
-
 /* obsolete */
 static int sysctl_tcp_low_latency __read_mostly;
 
