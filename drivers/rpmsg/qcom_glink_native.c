4b0982745a127ab176aacb5b6ac59a6c58414fb4
rmux <armandas.kvietkus@proton.me>
2026-07-13 19:54:20 +0200

    rpmsg: glink: use only lower 16-bits of param2 for CMD_OPEN name length

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -1263,7 +1263,7 @@ static irqreturn_t qcom_glink_native_intr(int irq, void *data)
 			qcom_glink_rx_advance(glink, ALIGN(sizeof(msg), 8));
 			break;
 		case RPM_CMD_OPEN:
-			ret = qcom_glink_rx_defer(glink, param2);
+			ret = qcom_glink_rx_defer(glink, param2 & 0xffff);
 			break;
 		case RPM_CMD_TX_DATA:
 		case RPM_CMD_TX_DATA_CONT:
